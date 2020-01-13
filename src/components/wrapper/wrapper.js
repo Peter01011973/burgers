25bfbe2db0dd0670d3fa87dc025555177386c58e
Petro Maliar <maliar@zfort.local>
2020-01-13 18:38:06 +0200

    one more commit

--- src/components/wrapper/wrapper.js
+++ src/components/wrapper/wrapper.js
@@ -4,16 +4,17 @@ import SliderArea from '../slider-area/slider-area';
 import BestBurgerArea from '../best-burger-area/best-burger-area';
 import PresidentBurgerArea from '../president-burger-area/president-burger-area';
 import AboutArea from '../about-area/about-area';
+import PreviousNextMethods from '../slider/slider';
 
 const Wrapper = (props) => {
     return(
         <React.Fragment>
-            <div className = 'empty-div'></div>
             {props.children}
             <SliderArea />
             <BestBurgerArea />
             <PresidentBurgerArea />
             <AboutArea />
+            <PreviousNextMethods />
         </React.Fragment>
     )
 }
