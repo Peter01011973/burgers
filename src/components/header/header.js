7f89454eecc640f38e728f93fbe10f496007f52c
Peter <petr_adamovich@i.ua>
2020-01-16 23:39:37 +0200

    adapted

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,45 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './header.css';
 import logo from '../../assets/image/logo.png';
 import NavMenu from './nav-menu/nav-menu';
 import SocailLinkMenu from '../header/social-link-menu/social-link-menu';
 import DrawerToggleButton from './drawerToggleButton/drawerToggleButton';
 
-const mobileMenu = () => {
-    console.log('gfgfgf');
-    
-    return (
-        <NavMenu render = {'Yes'}/>
-    )
-}
+export default class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showMobileMenu: false
+        }
+    }
 
-const Header = () => {
-    return (
-        <header className="header">
+    mobileMenuRender = () => {
+        this.setState((state)=>({showMobileMenu: !state.showMobileMenu}))
+        // return (<NavMenu />)
+    }
 
-            <div className='header__row'>
-                <div className="header__row-navigation">
-                    <NavMenu />
+    render() {
+        return (
+            <header className="header">
+
+                <div className='header__row'>
+                    <div className="header__row-navigation">
+                        <NavMenu />
+                    </div>
+                    <div className='header__row-logo'>
+                        <a className="logo-img" href="#"><img className='logo_img' src={logo} alt="logo" /></a>
+                    </div>
+                    <div className="header__row-info">
+                        <div className="socail_links">
+                            <SocailLinkMenu />
+                        </div>
+                        <div className="book_btn">
+                            <a className="#" href="#">+10 367 453 7382</a>
+                        </div>
+                    </div>
                 </div>
-                <div header__row-logo>
+                <div className='mobile-menu'>
                     <a className="logo-img" href="#"><img className='logo_img' src={logo} alt="logo" /></a>
+                    <DrawerToggleButton className='btn' clickHandle={this.mobileMenuRender} />
+                    {this.state.showMobileMenu ? <NavMenu />: null}
                 </div>
-                <div className="header__row-info">
-                    <div className="socail_links">
-                        <SocailLinkMenu />
-                    </div>
-                    <div className="book_btn">
-                        <a className="#" href="#">+10 367 453 7382</a>
-                    </div>
-                </div>
-            </div>
-            <div className='mobile-menu'>
-                <a className="logo-img" href="#"><img className='logo_img' src={logo} alt="logo" /></a>
-                <DrawerToggleButton/>
-                <a className='btn' onClick = {mobileMenu}><i className="fa fa-bars"></i></a>
-            </div>
-        </header>
-    )
+            </header>
+        )
+    }
 }
 
-export default Header;
